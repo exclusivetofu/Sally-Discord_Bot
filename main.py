be0f5479a87009e04a7cee56571a588206c703f6
Tofu133 <exclusivetofu@gmail.com>
2021-12-09 16:42:42 -0700

    A Test Bot Program
    must be replaced with sally's new code

---
+++ main.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ui import Button, View
+from discord.ext import commands
+
+Sally = commands.Bot(command_prefix='*')
+
+@Sally.event
+async def on_ready():
+    print(f'{Sally.user} Ready!')
+
+@Sally.command()
+async def ping(ctx):
+    button1 = Button(label='SUCK', style=discord.ButtonStyle.green)
+    button2 = Button(label='MY'  , style=discord.ButtonStyle.blurple)
+    button3 = Button(style=discord.ButtonStyle.red, emoji='🍆')
+    view = View(button1, button2, button3)
+
+    await ctx.send('.', view= view)
+
+from config import TOKEN
+Sally.run(TOKEN)
