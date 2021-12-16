9b79e4e88209acfaed8ebb4c9c347c446538a2af
exclusivetofu <exclusivetofu@gmail.com>
2021-12-16 00:54:32 -0700

    added a channel that shows time of the server

---
+++ Cogs/ServerTimeCog.py
@@ -0,0 +1,25 @@
+from discord.ext import tasks, commands
+import discord
+from datetime import datetime
+
+class ServerTime(commands.Cog):
+    def __init__(self, bot, timeChat):
+        self.bot = bot
+        self.update.start()
+        self.timeChat = timeChat
+
+    def cog_unload(self):
+        self.update.cancel()
+
+    @tasks.loop(seconds=60.0)
+    async def update(self):
+
+        if len(self.bot.guilds) == 0:
+            print('Time Channel Not Ready')
+            return
+        
+        now = datetime.now()
+        time = now.strftime("local-time-%H-%M")
+        print(time)
+        await self.timeChat.edit(name=time)
+        #print(self.bot.guilds)
--- Sally.py
+++ Sally.py
@@ -8,6 +8,7 @@ Sally = commands.Bot(command_prefix = '*',
                      intents = SallysIntents,
                      status = discord.Status.online
                     )
+timeChat = ''
 
 @Sally.event
 async def on_ready():
@@ -15,6 +16,10 @@ async def on_ready():
     from users import TOFU 
     Tofu = discord.utils.find(lambda Tofu: Tofu.id == TOFU, Sally.guilds[0].members)
     await Tofu.send('Awating orders master')
+    timeChat = discord.utils.find(lambda c: c.id == 920933193383813181, 
+                                      Sally.guilds[0].channels)
+    from Cogs.ServerTimeCog import ServerTime
+    Sally.add_cog(ServerTime(Sally, timeChat))
 
 from Cogs.DebugCog import DebugCog
 Sally.add_cog(DebugCog(Sally))
@@ -27,6 +32,8 @@ Sally.add_cog(MemverCog(Sally))
 
 from Cogs.RequestCog import RequestCog
 Sally.add_cog(RequestCog(Sally))
+
+
 
 with open('token.txt') as File:
     Sally.run(File.read())
